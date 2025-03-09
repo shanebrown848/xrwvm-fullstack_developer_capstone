c1e847b22f5add29a138887b0e4cf6d6d91c6100
shanebrown848 <shanebrown848@gmail.com>
2025-03-09 13:59:00 -0400

    Add CarMake and CarModel models with admin registration

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,4 +1,40 @@
 # Uncomment the following imports before adding the Model code
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any other fields you would like
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
+
+# Car Model model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # One CarMake can have many CarModels
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # You can add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    # Add any other fields you would like
+
+    def __str__(self):
+        # You can customize this to display both make and model if desired
+        return f"{self.car_make.name} {self.name}"
 
 # from django.db import models
 # from django.utils.timezone import now
